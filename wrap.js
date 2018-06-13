f3ee08135c42692b4ef22d3e2331b37528dfeba3
Lars Trieloff <trieloff@adobe.com>
2018-06-13 15:40:28 +0000

    More verbose logging in wrapper, more robust way to get environment variables

--- wrap.js
+++ wrap.js
@@ -22,100 +22,114 @@ const _ = require("lodash/fp");
  * inadvertantly echoed
  */
 function sanitize(o) {
-    return _.fromPairs(
-      Object.entries(o).map(([k, v]) => {
-        if (k.match(/^[A-Z0-9_]+$/)) {
-          return [k, k.replace(/./g, "•") + v.substr(-4)];
-        } else {
-          return [k, v];
-        }
-      })
-    );
-  }
-  /**
-   * Returns true when k is likely the name of a secure key (i.e. in ALL_CAPS)
-   * @param {String} k
-   */
-  function secure(v, k) {
-    return k.match(/^[A-Z0-9_]+$/);
-  }
-  
-  function split(o) {
-    return [_.pickBy(secure, o), _.pickBy(_.negate(secure), o)];
-  }
-  
-  /**
-   * Wraps a function f with proper logging, before and after.
-   */
-  function wrap(f) {
-    return function(p) {
-      const winston = require("winston");
-      require("winston-loggly-bulk");
-  
-      // default log level
-      var loglevel = "debug";
-      // default request ID
-      var requestid = "debug";
-      // OpenWhisk activation ID (works only in OpenWhisk, only for a single invocation)
-      var activation = process.env["__OW_ACTIVATION_ID"] ? process.env["__OW_ACTIVATION_ID"] : "debug";
-
-      var functionname = process.env["__OW_ACTION_NAME"] ? process.env["__OW_ACTION_NAME"].replace(/\/.+\//, "") : "debug";
-
-      if (p["__ow_headers"]) {
-        if (p["__ow_headers"]["X-Debug"]) {
-          // use the log level set in the `X-Debug` header
-          loglevel = p["__ow_headers"]["X-Debug"];
-          if (p["__ow_headers"]["X-CDN-Request-Id"]) {
-              requestid = p["__ow_headers"]["X-CDN-Request-Id"];
-          }
-        }
+  return _.fromPairs(
+    Object.entries(o).map(([k, v]) => {
+      if (k.match(/^[A-Z0-9_]+$/)) {
+        return [k, k.replace(/./g, "•") + v.substr(-4)];
       } else {
-        //there are no headers present, this is a direct invocation, set log level to debug
-        loglevel = "debug";
+        return [k, v];
       }
-  
-      console.log("Logging with level " + loglevel);
-      try {
-        winston.add(winston.transports.Loggly, {
-          token: p.LOGGLY_KEY,
-          subdomain: p.LOGGLY_HOST,
-          //include OW_ACTION_NAME in tags for easier filtering
-          tags: [
-            "OpenWhisk",
-            functionname,
-            activation
-          ],
-          json: true,
-          level: loglevel
-        });
-      } catch (e) {
-        if (!e.toString().indexOf('Transport already attached')) {
-          console.error("ERROR in wrap", e);
-        }
-      }
-      console.log("before");
-      const [secrets, params] = split(p);
-      winston.log("debug", "before", { params: params, request: requestid, activation: activation, function: process.env["__OW_ACTION_NAME"] });
-      try {
-        const retval = Promise.resolve(f(params, secrets, winston))
-          .then(r => {
-            console.log("resolved");
-            winston.log("debug", "resolved", { result: r, request: requestid, activation: activation, function: process.env["__OW_ACTION_NAME"] });
-            return r;
-          })
-          .catch(e => {
-            console.log("error");
-            winston.log("debug", "error", { error: e, request: requestid, activation: activation, function: process.env["__OW_ACTION_NAME"] });
-            return e;
-          });
-        console.log('returning');
-        return retval;
-      } catch (e) {
-        if (!e.toString().indexOf('Transport already attached')) {
-          console.error("ERROR in wrap", e);
-        }
-      }
-    };
-  }
+    })
+  );
+}
+/**
+ * Returns true when k is likely the name of a secure key (i.e. in ALL_CAPS)
+ * @param {String} k
+ */
+function secure(v, k) {
+  return k.match(/^[A-Z0-9_]+$/);
+}
 
-  module.exports = wrap;
+function split(o) {
+  return [_.pickBy(secure, o), _.pickBy(_.negate(secure), o)];
+}
+
+/**
+ * Wraps a function f with proper logging, before and after.
+ */
+function wrap(f) {
+  return function(p) {
+    const winston = require("winston");
+    require("winston-loggly-bulk");
+
+    // default log level
+    var loglevel = "debug";
+    // default request ID
+    var requestid = "debug";
+    // OpenWhisk activation ID (works only in OpenWhisk, only for a single invocation)
+    var activation = process.env["__OW_ACTIVATION_ID"]
+      ? process.env["__OW_ACTIVATION_ID"]
+      : "debug";
+
+    var functionname = process.env["__OW_ACTION_NAME"]
+      ? process.env["__OW_ACTION_NAME"].replace(/\/.+\//, "")
+      : "debug";
+
+    if (p["__ow_headers"]) {
+      if (p["__ow_headers"]["X-Debug"]) {
+        // use the log level set in the `X-Debug` header
+        loglevel = p["__ow_headers"]["X-Debug"];
+        if (p["__ow_headers"]["X-CDN-Request-Id"]) {
+          requestid = p["__ow_headers"]["X-CDN-Request-Id"];
+        }
+      }
+    } else {
+      //there are no headers present, this is a direct invocation, set log level to debug
+      loglevel = "debug";
+    }
+
+    console.log("Logging with level " + loglevel);
+    try {
+      winston.add(winston.transports.Loggly, {
+        token: p.LOGGLY_KEY,
+        subdomain: p.LOGGLY_HOST,
+        //include OW_ACTION_NAME in tags for easier filtering
+        tags: ["OpenWhisk", functionname, activation],
+        json: true,
+        level: loglevel
+      });
+    } catch (e) {
+      if (!e.toString().indexOf("Transport already attached")) {
+        console.error("ERROR in wrap", e);
+      }
+    }
+    console.log("before");
+    const [secrets, params] = split(p);
+    winston.log("debug", "before", {
+      params: params,
+      request: requestid,
+      activation: activation,
+      function: process.env["__OW_ACTION_NAME"]
+    });
+    try {
+      const retval = Promise.resolve(f(params, secrets, winston))
+        .then(r => {
+          console.log("resolved");
+          winston.log("debug", "resolved", {
+            result: r,
+            request: requestid,
+            activation: activation,
+            function: process.env["__OW_ACTION_NAME"]
+          });
+          return r;
+        })
+        .catch(e => {
+          console.log("error");
+          winston.log("debug", "error", {
+            error: e,
+            request: requestid,
+            activation: activation,
+            function: process.env["__OW_ACTION_NAME"]
+          });
+          return e;
+        });
+      console.log("returning");
+      return retval;
+    } catch (e) {
+      winston.log("error", e);
+      return { error: e };
+    }
+  };
+}
+
+module.exports = wrap;

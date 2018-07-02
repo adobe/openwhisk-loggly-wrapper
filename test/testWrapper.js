044b2276e32014da81450457383bfc28ad84f838
Lars Trieloff <trieloff@adobe.com>
2018-07-02 11:34:49 +0000

    ignore generated files

--- test/testWrapper.js
+++ test/testWrapper.js
@@ -1,93 +1,90 @@
-const assert = require("assert");
-const wrapper = require("../wrap");
+/*
+ * Copyright 2018 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+const assert = require('assert');
+const wrapper = require('../wrap');
 
-describe("Test wrapper.js", () => {
-  it("Wrapper can be loaded", () => {
+describe('Test wrapper.js', () => {
+  it('Wrapper can be loaded', () => {
     assert.ok(wrapper);
   });
 
-  it("Wrapper is a function", () => {
-    assert.equal(typeof wrapper, "function");
+  it('Wrapper is a function', () => {
+    assert.equal(typeof wrapper, 'function');
   });
 
-  it("Wrapping executes immediately", done => {
-    assert.ok(
-      wrapper(() => {
-        done();
-        return true;
-      })
-    );
+  it('Wrapping executes immediately', (done) => {
+    assert.ok(wrapper(() => {
+      done();
+      return true;
+    }));
   });
 
-  it("Wrapping passes parameters", done => {
-    assert.ok(
-      wrapper(p => {
-        assert.ok(p.hello);
-        done();
-        return true;
-      }, {hello: 'world'})
-    );
+  it('Wrapping passes parameters', (done) => {
+    assert.ok(wrapper((p) => {
+      assert.ok(p.hello);
+      done();
+      return true;
+    }, { hello: 'world' }));
   });
 
-  it("Wrapping masks secret parameters", done => {
-    assert.ok(
-      wrapper(p => {
-        assert.notEqual(p.HELLO, 'world');
-        done();
-        return true;
-      }, {HELLO: 'world'})
-    );
+  it('Wrapping masks secret parameters', (done) => {
+    assert.ok(wrapper((p) => {
+      assert.notEqual(p.HELLO, 'world');
+      done();
+      return true;
+    }, { HELLO: 'world' }));
   });
 
-  it("Wrapping passes secret parameters as secrets", done => {
-    assert.ok(
-      wrapper((p, s) => {
-        assert.notEqual(p.HELLO, 'world');
-        assert.equal(s.HELLO, 'world');
-        done();
-        return true;
-      }, {HELLO: 'world'})
-    );
+  it('Wrapping passes secret parameters as secrets', (done) => {
+    assert.ok(wrapper((p, s) => {
+      assert.notEqual(p.HELLO, 'world');
+      assert.equal(s.HELLO, 'world');
+      done();
+      return true;
+    }, { HELLO: 'world' }));
   });
 
-  it("Wrapping enables overriding of secret parameters", done => {
-    assert.ok(
-      wrapper((p, s) => {
-        assert.notEqual(s.HELLO, 'world');
-        assert.equal(s.HELLO, 'just kidding');
-        done();
-        return true;
-      }, {HELLO: 'world'}, {HELLO: 'just kidding'})
-    );
+  it('Wrapping enables overriding of secret parameters', (done) => {
+    assert.ok(wrapper((p, s) => {
+      assert.notEqual(s.HELLO, 'world');
+      assert.equal(s.HELLO, 'just kidding');
+      done();
+      return true;
+    }, { HELLO: 'world' }, { HELLO: 'just kidding' }));
   });
 
-  it("Wrapping sets up a logger with one transports", done => {
-    assert.ok(
-      wrapper((p, s, l) => {
-        assert.ok(l);
-        assert.ok(l.loggers.default.transports);
-        assert.equal(l.loggers.default.transports.length, 1);
-        done();
-        return true;
-      }, {HELLO: 'world'}, {HELLO: 'just kidding'})
-    );
+  it('Wrapping sets up a logger with one transports', (done) => {
+    assert.ok(wrapper((p, s, l) => {
+      assert.ok(l);
+      assert.ok(l.loggers.default.transports);
+      assert.equal(l.loggers.default.transports.length, 1);
+      done();
+      return true;
+    }, { HELLO: 'world' }, { HELLO: 'just kidding' }));
   });
 
-  it("Wrapping passes the provided logger", done => {
+  it('Wrapping passes the provided logger', (done) => {
     const mylogger = require('winston');
 
-    assert.ok(
-      wrapper((p, s, l) => {
-        assert.ok(l);
-        assert.strictEqual(l, mylogger);
-        l.log('info', 'Testing…');
-        done();
-        return true;
-      }, {HELLO: 'world'}, {HELLO: 'just kidding'}, mylogger)
-    );
+    assert.ok(wrapper((p, s, l) => {
+      assert.ok(l);
+      assert.strictEqual(l, mylogger);
+      l.log('info', 'Testing…');
+      done();
+      return true;
+    }, { HELLO: 'world' }, { HELLO: 'just kidding' }, mylogger));
   });
 
-  it("Spreading args works", done => {
+  it('Spreading args works', (done) => {
     let counter = 0;
 
     const functiontorun = (params, secrets, logger) => {
@@ -95,7 +92,7 @@ describe("Test wrapper.js", () => {
       assert.equal(counter, 2);
       assert.equal(params.hey, 'ho');
       done();
-    }
+    };
 
     const wrapped = (...args) => require('../wrap')(functiontorun, ...args);
 
@@ -104,6 +101,6 @@ describe("Test wrapper.js", () => {
 
     counter++;
     assert.equal(counter, 1);
-    wrapped({hey: 'ho'});
-  })
+    wrapped({ hey: 'ho' });
+  });
 });
--- wrap.js
+++ wrap.js
@@ -1,21 +1,15 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * Copyright 2018 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
  */
-const _ = require("lodash/fp");
+const _ = require('lodash/fp');
 
 /**
  * Returns true when k is likely the name of a secure key (i.e. in ALL_CAPS)
@@ -30,52 +24,52 @@ function split(o) {
 }
 
 function loglevel(p = {}) {
-  if (p["__ow_headers"]) {
-    if (p["__ow_headers"]["X-Debug"]) {
+  if (p.__ow_headers) {
+    if (p.__ow_headers['X-Debug']) {
       // use the log level set in the `X-Debug` header
-      return p["__ow_headers"]["X-Debug"];
+      return p.__ow_headers['X-Debug'];
     }
   }
-  return "debug";
+  return 'debug';
 }
 
 function activationid() {
-  return process.env["__OW_ACTIVATION_ID"]
-    ? process.env["__OW_ACTIVATION_ID"]
-    : "debug";
+  return process.env.__OW_ACTIVATION_ID
+    ? process.env.__OW_ACTIVATION_ID
+    : 'debug';
 }
 
 function functionname() {
-  return process.env["__OW_ACTION_NAME"]
-    ? process.env["__OW_ACTION_NAME"].replace(/\/.+\//, "")
-    : "debug";
+  return process.env.__OW_ACTION_NAME
+    ? process.env.__OW_ACTION_NAME.replace(/\/.+\//, '')
+    : 'debug';
 }
 
 function requestid(p = {}) {
-  if (p["__ow_headers"]) {
-    if (p["__ow_headers"]["X-CDN-Request-Id"]) {
-      return p["__ow_headers"]["X-CDN-Request-Id"];
+  if (p.__ow_headers) {
+    if (p.__ow_headers['X-CDN-Request-Id']) {
+      return p.__ow_headers['X-CDN-Request-Id'];
     }
   }
-  return "debug";
+  return 'debug';
 }
 
 function defaultlogger(p, secrets) {
-  const winston = require("winston");
-  require("winston-loggly-bulk");
+  const winston = require('winston');
+  require('winston-loggly-bulk');
 
   try {
     winston.add(winston.transports.Loggly, {
       token: secrets.LOGGLY_KEY || p.LOGGLY_KEY,
       subdomain: secrets.LOGGLY_HOST || p.LOGGLY_HOST,
-      //include OW_ACTION_NAME in tags for easier filtering
-      tags: ["OpenWhisk", functionname(), activationid()],
+      // include OW_ACTION_NAME in tags for easier filtering
+      tags: ['OpenWhisk', functionname(), activationid()],
       json: true,
-      level: loglevel(p)
+      level: loglevel(p),
     });
   } catch (e) {
-    if (!e.toString().indexOf("Transport already attached")) {
-      console.error("ERROR in wrap", e);
+    if (!e.toString().indexOf('Transport already attached')) {
+      console.error('ERROR in wrap', e);
     }
   }
   return winston;
@@ -88,40 +82,40 @@ function wrap(
   f,
   passedparams,
   passedsecrets,
-  logger = defaultlogger(passedparams, passedsecrets)
+  logger = defaultlogger(passedparams, passedsecrets),
 ) {
   const [parsedsecrets, params] = split(passedparams);
   // allow overriding secrets
   const secrets = Object.assign(parsedsecrets, passedsecrets);
-  logger.log("silly", "before", {
-    params: params,
+  logger.log('silly', 'before', {
+    params,
     request: requestid(params),
     activation: activationid(),
-    function: functionname()
+    function: functionname(),
   });
   try {
     const retval = Promise.resolve(f(params, secrets, logger))
-      .then(r => {
-        logger.log("silly", "resolved", {
+      .then((r) => {
+        logger.log('silly', 'resolved', {
           result: r,
           request: requestid(params),
           activation: activationid(),
-          function: functionname()
+          function: functionname(),
         });
         return r;
       })
-      .catch(e => {
-        logger.log("debug", "error", {
+      .catch((e) => {
+        logger.log('debug', 'error', {
           error: e,
           request: requestid(params),
           activation: activationid(),
-          function: functionname()
+          function: functionname(),
         });
         return e;
       });
     return retval;
   } catch (e) {
-    logger.log("error", e);
+    logger.log('error', e);
     return { error: e };
   }
 }
